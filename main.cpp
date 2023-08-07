b5eedf95e50400b939287bbfa561d01326480c06
Vexoben <2493583737@qq.com>
2023-08-07 22:08:33 +0800

    debug

--- main.cpp
+++ main.cpp
@@ -3,69 +3,26 @@
 #include <cstring>
 #include "bptree.hpp"
 
-struct String {
-    char index[65];
-
-    String(const String &other) {
-        for (int i = 0; i < 65; i++)index[i] = other.index[i];
-    }
-
-    String() = default;
-
-    friend bool operator>(const String &lhs, const String &rhs) {
-        return std::string(lhs.index) > std::string(rhs.index);
-    }
-
-    friend bool operator>=(const String &lhs, const String &rhs) {
-        return std::string(lhs.index) >= std::string(rhs.index);
-    }
-
-    friend bool operator<(const String &lhs, const String &rhs) {
-        return std::string(lhs.index) < std::string(rhs.index);
-    }
-
-    friend bool operator<=(const String &lhs, const String &rhs) {
-        return std::string(lhs.index) <= std::string(rhs.index);
-    }
-
-    friend bool operator==(const String &lhs, const String &rhs) {
-        return std::string(lhs.index) == std::string(rhs.index);
-    }
-
-    friend bool operator!=(const String &lhs, const String &rhs) {
-        return std::string(lhs.index) != std::string(rhs.index);
-    }
-
-    friend std::ostream &operator<<(std::ostream &os, const String &obj) {
-        os << obj.index;
-        return os;
-    }
-
-
-};
-
 int main() {
-   // freopen("5.in","r",stdin);
-   // freopen("me.out","w",stdout);
-    BPTree<String, int> bpTree("test");
-    std::pair<String, int> val;
+    BPTree<std::string, int> bpTree("test");
+    std::pair<std::string, int> val;
     int cnt;
     std::string cmd;
     scanf("%d", &cnt);
     for (int i = 1; i <= cnt; i++) {
         std::cin >> cmd;
         if (cmd[0] == 'i') {
-            scanf("%s%d", val.first.index, &val.second);
+            std::cin >> val.first >> val.second;
             bpTree.insert(val);
         } else if (cmd[0] == 'f') {
-            scanf("%s", val.first.index);
+            std::cin >> val.first;
             std::vector<int> ans = bpTree.find(val.first);
             if (!ans.empty()) {
                 for (int i = 0; i < ans.size() - 1; i++)printf("%d ", ans[i]);
                 printf("%d\n", ans[ans.size() - 1]);
             } else puts("null");
         } else if (cmd[0] == 'd') {
-            scanf("%s%d", val.first.index, &val.second);
+            std::cin >> val.first >> val.second;
             bpTree.remove(val);
         }
 
