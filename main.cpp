e88737e49943fe6f1a64c014ce8f6c10604893fc
Vexoben <2493583737@qq.com>
2023-08-07 22:10:57 +0800

    debug

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main() {
     std::pair<std::string, int> val;
     int cnt;
     std::string cmd;
-    scanf("%d", &cnt);
+    std::cin >> cnt;
     for (int i = 1; i <= cnt; i++) {
         std::cin >> cmd;
         if (cmd[0] == 'i') {
@@ -18,8 +18,8 @@ int main() {
             std::cin >> val.first;
             std::vector<int> ans = bpTree.find(val.first);
             if (!ans.empty()) {
-                for (int i = 0; i < ans.size() - 1; i++)printf("%d ", ans[i]);
-                printf("%d\n", ans[ans.size() - 1]);
+                for (int i = 0; i < ans.size() - 1; i++) std::cout << ans[i] << ' ';
+                std::cout << ans[ans.size() - 1] << std::endl;
             } else puts("null");
         } else if (cmd[0] == 'd') {
             std::cin >> val.first >> val.second;
