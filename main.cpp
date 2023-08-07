38eebe8fc86e5160c05b88efa820a0d8b5d7b2ba
Vexoben <2493583737@qq.com>
2023-08-07 22:02:28 +0800

    debug

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <cstdio>
 #include <cstring>
-#include "vector.hpp"
 #include "bptree.hpp"
 
 struct String {
@@ -60,7 +59,7 @@ int main() {
             bpTree.insert(val);
         } else if (cmd[0] == 'f') {
             scanf("%s", val.first.index);
-            sjtu::vector<int> ans = bpTree.Find(val.first);
+            std::vector<int> ans = bpTree.find(val.first);
             if (!ans.empty()) {
                 for (int i = 0; i < ans.size() - 1; i++)printf("%d ", ans[i]);
                 printf("%d\n", ans[ans.size() - 1]);
